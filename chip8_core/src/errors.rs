3552c93acc32dbe10899a55fb25f356d2243e713
Jonatan Søgaard <jonatanjuhl@icloud.com>
2025-03-13 23:28:31 +0100

    Refactored decoding match to primarily use ranges instead of bit masking

--- chip8_core/src/errors.rs
+++ chip8_core/src/errors.rs
@@ -5,6 +5,9 @@ pub enum Error {
     #[error("Program contained an invalid opcode:\n {0}")]
     InvalidOpcodeError(String),
 
+    #[error("Program contained an unknown opcode:\n {0:#X}")]
+    UnknownOpcodeError(u16),
+
     #[error("Invalid ROM size")]
     InvalidRomSizeError,
 
